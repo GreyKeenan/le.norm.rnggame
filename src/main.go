b1cc645d392b12e132b224b305bb8403c9a66360
GreyKeenan <156843326+GreyKeenan@users.noreply.github.com>
2025-03-31 21:16:55 -0500

    passing seed to program

--- src/main.go
+++ src/main.go
@@ -7,16 +7,32 @@ import (
 	"math/rand"
 	"time"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 
-	var t int64 = time.Now().Unix()
-	var src rand.Source = rand.NewSource(t)
+	var e error
+
+	var seed int64
+
+	if len(os.Args) > 1 {
+		seed, e = strconv.ParseInt(os.Args[1], 10, 63)
+		if (e != nil) {
+			seed = time.Now().Unix()
+		}
+	} else {
+		seed = time.Now().Unix()
+	}
+
+	// seed with immediate '21' loss: 1743473300
+
+	var src rand.Source = rand.NewSource(seed)
 	var rzer *rand.Rand = rand.New(src)
 
 	print("\033[H\033[2J")
-	fmt.Printf("\nseed: %d\n\n", t)
+	fmt.Printf("\nseed: %d\n\n", seed)
 	game.Play_cli(rzer)
 	fmt.Println()
 
